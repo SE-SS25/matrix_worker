c6c77c75d2ee403a57dcc578c7113d9579099e03
HaDeSMonsta <leon.poellinger+git@gmail.com>
2025-06-20 23:40:18 +0200

    Add example query

--- matrix-db_manager/src/lib.rs
+++ matrix-db_manager/src/lib.rs
@@ -1,9 +1,12 @@
 use anyhow::{Context, Result};
 use matrix_macros::get_env;
 use sqlx::postgres::PgPoolOptions;
-use sqlx::{Postgres, migrate};
+use sqlx::postgres::types::PgInterval;
+use sqlx::types::chrono;
+use sqlx::{Postgres, migrate, query};
 use std::time::Duration;
 use tracing::{debug, info, instrument};
+use uuid::Uuid;
 
 pub type DbType = Postgres;
 pub type DbPool = sqlx::Pool<DbType>;
@@ -29,6 +32,52 @@ pub async fn init() -> Result<DbPool> {
     info!("Connected to database");
 
     Ok(pool)
+}
+
+#[instrument(skip_all)]
+pub async fn worker_metric_example(db_pool: &DbPool) -> Result<()> {
+    let now = chrono::Utc::now();
+    let id = Uuid::new_v4();
+    let uptime = PgInterval {
+        months: 0,
+        days: 0,
+        microseconds: 300,
+    };
+    query!(
+        r#"
+        DELETE FROM worker_metric
+            WHERE TRUE;
+        "#
+    )
+    .execute(db_pool)
+    .await
+    .context("Can't delete")?;
+    query!(
+        r#"
+        INSERT INTO worker_metric (id, last_heartbeat, uptime)
+            VALUES ($1, $2, $3);
+        "#,
+        id,
+        now,
+        uptime,
+    )
+    .execute(db_pool)
+    .await
+    .context("Can't insert")?;
+
+    let res = query!(
+        r#"
+        SELECT * FROM worker_metric
+            LIMIT 1;
+        "#
+    )
+    .fetch_one(db_pool)
+    .await
+    .context("Can't get worker_metrics")?;
+
+    info!("{res:#?}");
+
+    Ok(())
 }
 
 #[instrument(skip_all)]
