4bc2f2bf17f713978ff8e7c7b2ca23810aa5e601
HaDeSMonsta <leon.poellinger+git@gmail.com>
2025-06-23 11:33:02 +0200

    Properly calculate uptime

--- matrix-db_manager/src/metrics_manager.rs
+++ matrix-db_manager/src/metrics_manager.rs
@@ -6,11 +6,11 @@ use sqlx::query;
 use sqlx::types::chrono;
 use std::time::{Duration, Instant};
 use tokio::time::sleep;
-use tracing::{debug, error, instrument};
+use tracing::{debug, error, instrument, warn};
 
 const PERSIST_INTERVAL: Duration = Duration::from_secs(10);
 
-#[instrument(skip_all)]
+#[instrument(name = "manage metrics", skip_all)]
 pub async fn manage(metrics: MetricsWrapper, db_pool: DbPool) {
     let startup = Instant::now();
     loop {
@@ -26,17 +26,14 @@ pub async fn manage(metrics: MetricsWrapper, db_pool: DbPool) {
 async fn persist(metrics: &MetricsWrapper, running_since: Instant, db_pool: &DbPool) -> Result<()> {
     let id = metrics.id();
     let last_heartbeat = chrono::Utc::now();
-    let uptime = PgInterval {
-        months: 0,
-        days: 0,
-        microseconds: (Instant::now() - running_since).as_micros() as i64,
-    };
+    let uptime = instant_to_interval(running_since);
     let read_per_sec = metrics.read_ps() as i32;
     let write_per_sec = metrics.write_ps() as i32;
     let req_per_sec = read_per_sec + write_per_sec;
     let req_total = metrics.get_total_requests() as i64;
     let req_failed = metrics.get_total_fails() as i64;
     let db_avail = (req_failed as f32) / (req_total as f32);
+
     query!(
         r#"
         INSERT INTO worker_metric
@@ -75,5 +72,25 @@ async fn persist(metrics: &MetricsWrapper, running_since: Instant, db_pool: &DbP
     .execute(db_pool)
     .await
     .context("Unable to persist metrics")?;
+
     Ok(())
+}
+
+#[instrument(skip_all)]
+fn instant_to_interval(ts: Instant) -> PgInterval {
+    let total_micros = Instant::now().duration_since(ts).as_micros() as i64; // i64 is cringe
+
+    const MICROS_PER_DAY: i64 = 24 * 60 * 60 * 1_000_000;
+    const DAYS_PER_MONTH: i32 = 30; // Because I say so, problem?
+
+    let total_days = (total_micros / MICROS_PER_DAY) as i32; // Anybody else with Prog1 flashbacks?
+    let months = total_days / DAYS_PER_MONTH;
+    let days = total_days % DAYS_PER_MONTH;
+    let microseconds = total_micros % MICROS_PER_DAY;
+
+    PgInterval {
+        months,
+        days,
+        microseconds,
+    }
 }
