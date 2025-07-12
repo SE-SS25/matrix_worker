eda3228fb6c72a36aefb94a813d851cb3dfb6f44
HaDeSMonsta <leon.poellinger+git@gmail.com>
2025-07-12 18:04:07 +0200

    Update jitter function

--- matrix-commons/src/lib.rs
+++ matrix-commons/src/lib.rs
@@ -7,12 +7,9 @@ pub const MAX_BACKOFF: u128 = 5 * 60 * 1_000; // Max 5 mins
 
 pub fn jitter(current: Duration) -> Duration {
     let current_millis = current.as_millis();
-    if current_millis >= MAX_BACKOFF {
-        return Duration::from_millis(MAX_BACKOFF as u64);
-    }
+    let new_millis = (current_millis * 2).min(MAX_BACKOFF);
 
-    let backoff_millis =
-        rand::random_range(current_millis..MAX_BACKOFF.min(current_millis * 2)) as u64;
+    let backoff_millis = rand::random_range(0..new_millis) as u64;
 
     Duration::from_millis(backoff_millis)
 }
